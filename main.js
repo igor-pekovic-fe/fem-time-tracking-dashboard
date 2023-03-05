502dc6d16eae6c1a8fd71164566b8bb378dd54c3
Igor Pekovic <igor.pekovic95@gmail.com>
2023-03-05 20:07:29 +0100

    Added logic for generating card information

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
 import * as data from "./data.js";
 
-// const data = fetch("./data.json")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error));
+const cards = document.querySelectorAll(".card");
 
-console.log(data);
+const dailyBtn = document.querySelector(".timeframe-daily");
+const weeklyBtn = document.querySelector(".timeframe-weekly");
+const monthlyBtn = document.querySelector(".timeframe-monthly");
+
+dailyBtn.addEventListener("click", () => renderTimeframe("daily"));
+weeklyBtn.addEventListener("click", () => renderTimeframe("weekly"));
+monthlyBtn.addEventListener("click", () => renderTimeframe("monthly"));
+
+function resetCard() {
+  cards.forEach((card) => (card.innerHTML = ""));
+}
+
+function renderTimeframe(timeframe) {
+  resetCard();
+  for (const card of [...cards]) {
+    const {
+      title = "self-care",
+      timeframes: {
+        [timeframe]: { current, previous },
+      },
+    } = data.default[[...cards].indexOf(card)];
+    card.insertAdjacentHTML(
+      "afterbegin",
+      `
+        <img src="./images/icon-${title
+          .toLowerCase()
+          .replace(" ", "-")}.svg" alt="${title} icon" class="card-icon" />
+        <h3 class="card-title">${title}</h3>
+        <p class="card-play-current current">${current}</p>
+        <p class="card-play-previous previous">${previous}</p>
+      `
+    );
+  }
+}
+
+renderTimeframe("daily");
