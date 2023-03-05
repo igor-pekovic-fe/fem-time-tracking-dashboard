41fc30e82bbfadd7c898a318de66cd64c8243d0b
Igor Pekovic <igor.pekovic95@gmail.com>
2023-03-05 11:58:15 +0100

    Removed dummy text in index.html, connected main script file and changed data.json into data.js in order to import data into main script file

---
+++ main.js
@@ -0,0 +1,8 @@
+import * as data from "./data.js";
+
+// const data = fetch("./data.json")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+console.log(data);
